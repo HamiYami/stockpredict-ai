c14350a80d97520c9257c37ce45f8715ad732f59
Muhammad Ahmad <muhammad.ahmad777e@gmail.com>
2026-04-28 12:32:31 +0500

    Use weights-only model loading

--- predictor/ml_service.py
+++ predictor/ml_service.py
@@ -3,36 +3,43 @@ import numpy as np
 import yfinance as yf
 import joblib
 
-from tensorflow.keras.models import load_model
-
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dropout, Dense, Input
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MODEL_PATH = os.path.join(BASE_DIR, "ml_models", "aapl_clean_model.h5")
 SCALER_PATH = os.path.join(BASE_DIR, "ml_models", "aapl_scaler.save")
+WEIGHTS_PATH = os.path.join(BASE_DIR, "ml_models", "aapl_weights.weights.h5")
 
-
-model = load_model(MODEL_PATH, compile=False)
+# Only scaler global (lightweight)
 scaler = joblib.load(SCALER_PATH)
 
 
 def predict_aapl():
 
+    # Load model ONLY when endpoint is called
+    model = Sequential([
+        Input(shape=(60,1)),
+        LSTM(50, return_sequences=True),
+        Dropout(0.2),
+        LSTM(50),
+        Dropout(0.2),
+        Dense(1)
+    ])
+
+    model.load_weights(WEIGHTS_PATH)
+
     data = yf.download("AAPL", period="90d")
     data.columns = data.columns.get_level_values(0)
 
     close_prices = data[['Close']]
-
     scaled = scaler.transform(close_prices)
 
     last_60 = scaled[-60:]
-
     future_predictions = []
-
     current_batch = last_60.copy()
 
     for _ in range(7):
-
         pred = model.predict(
             current_batch.reshape(1, 60, 1),
             verbose=0
@@ -59,10 +66,10 @@ def predict_aapl():
         signal = "HOLD"
 
     return {
-    "ticker": "AAPL",
-    "current_price": round(current_price, 2),
-    "history": close_prices["Close"].tail(60).round(2).tolist(),
-    "predictions": future_predictions.flatten().round(2).tolist(),
-    "signal": signal,
-    "change_percent": round(change, 2)
-}
+        "ticker": "AAPL",
+        "current_price": round(current_price, 2),
+        "history": close_prices["Close"].tail(60).round(2).tolist(),
+        "predictions": future_predictions.flatten().round(2).tolist(),
+        "signal": signal,
+        "change_percent": round(change, 2)
+    }
